429670c95797d00716306642ffaf2ab24433009e
Emi-R <88684755+Emi-R@users.noreply.github.com>
2022-09-13 15:55:52 -0300

    1

--- 2.2 Joins/ScriptsJoins.sql
+++ 2.2 Joins/ScriptsJoins.sql
@@ -45,7 +45,8 @@ Inner Join Pedidos P On C.ID = P.IDCliente
 Where P.Costo > 1000000
 Go
 
--- Listar IDPedido, Costo, Fecha de solicitud y fecha de finalización, descripción del producto, costo y apellido y nombre del cliente. Sólo listar aquellos registros de pedidos que hayan sido pagados.
+-- Listar IDPedido, Costo, Fecha de solicitud y fecha de finalización, descripción del producto, costo y apellido y nombre del cliente. 
+-- Sólo listar aquellos registros de pedidos que hayan sido pagados.
 Select 
     P.ID As 'ID Pedido', 
     P.Costo, 
@@ -61,7 +62,8 @@ Inner Join Productos Prod On P.IDProducto = Prod.ID
 Where P.Pagado = 1
 Go
 
--- Listar IDPedido, Fecha de solicitud, fecha de finalización, días de construcción del producto, días de construcción del pedido (fecha de finalización - fecha de solicitud) y una columna llamada Tiempo de construcción con la siguiente información: 
+-- Listar IDPedido, Fecha de solicitud, fecha de finalización, días de construcción del producto, días de construcción del pedido 
+-- (fecha de finalización - fecha de solicitud) y una columna llamada Tiempo de construcción con la siguiente información: 
 -- 'Con anterioridad' → Cuando la cantidad de días de construcción del pedido sea menor a los días de construcción del producto.
 -- 'Exacto'' → Si la cantidad de días de construcción del pedido y el producto son iguales
 -- 'Con demora' → Cuando la cantidad de días de construcción del pedido sea mayor a los días de construcción del producto.
@@ -80,21 +82,36 @@ From Pedidos P
 Inner Join Productos Prod On P.IDProducto = Prod.ID
 Go
 
--- Listar por cada cliente el apellido y nombres y los nombres de las categorías de aquellos productos de los cuales hayan realizado pedidos. No deben figurar registros duplicados.
+-- Listar por cada cliente el apellido y nombres y los nombres de las categorías de aquellos productos de los cuales hayan realizado pedidos. 
+-- No deben figurar registros duplicados.
 
-Select Distinct C.Apellidos, C.Nombres, Cat.Nombre From Clientes C
+Select Distinct
+    C.Apellidos,
+    C.Nombres,
+    Cat.Nombre
+From Clientes C
 Inner Join Pedidos P On C.ID = P.IDCliente
 Inner Join Productos Pr On P.IDProducto = Pr.ID
 Inner Join Categorias Cat On Pr.IDCategoria = Cat.ID
 
--- Listar apellidos y nombres de aquellos clientes que hayan realizado algún pedido cuya cantidad sea exactamente igual a la cantidad considerada mayorista del producto.
+-- Listar apellidos y nombres de aquellos clientes que hayan realizado algún pedido cuya cantidad sea exactamente igual 
+-- a la cantidad considerada mayorista del producto.
 
-Select Distinct C.Apellidos, C.Nombres From Clientes C
+Select Distinct 
+    C.Apellidos,
+    C.Nombres
+From Clientes C
 Inner Join Pedidos P On P.IDCliente = C.ID
 Inner Join Productos Pr On Pr.ID = P.IDProducto
 Where P.Cantidad = Pr.CantidadMayorista
 
--- Listar por cada producto el nombre del producto, el nombre de la categoría, el precio de venta minorista, el precio de venta mayorista y el porcentaje de ahorro que se obtiene por la compra mayorista a valor mayorista en relación al valor minorista.
+-- Listar por cada producto el nombre del producto, el nombre de la categoría, el precio de venta minorista, el precio de venta mayorista y el porcentaje de 
+-- ahorro que se obtiene por la compra mayorista a valor mayorista en relación al valor minorista.
 
-Select P.Descripcion, C.Nombre, P.Precio, P.PrecioVentaMayorista, (P.Precio - P.PrecioVentaMayorista) * 100 / P.Precio as 'Porcentaje de Ahorro' From Productos P
+Select 
+    P.Descripcion,
+    C.Nombre,
+    P.Precio,
+    P.PrecioVentaMayorista, (P.Precio - P.PrecioVentaMayorista) * 100 / P.Precio as 'Porcentaje de Ahorro'
+From Productos P
 Inner Join Categorias C On C.ID = P.IDCategoria
